5cc912af7f05cc09a24f5f0f00b56ebfcfc30f81
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2025-12-02 09:10:36 +0545

    Task 2 completed

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -7,8 +7,10 @@
             Rectangle rec = new Rectangle();
             double l = 10;
             double b = 20;
+
             rec.Length = l;
             rec.Breadth = b;
+
             Console.WriteLine("Area: " + rec.GetArea());
             Console.WriteLine("Perimeter: " + rec.GetPerimeter());
             Console.WriteLine(rec.ShowDetails());
---
+++ Task-2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+public delegate int Calculate(int a, int b);
+public delegate double DiscountStrategy(double price);
+public class Program { 
+    public static int Add(int a, int b) => a + b;
+    
+    public static int Subtract(int  a, int b) => a - b;
+
+    public static double FestivalDiscount(double p) => p - ( p * 0.2);
+    public static double SeasonalDiscount(double p) => p - (p * 0.1);
+
+    public static double NoDiscount(double p) => p;
+
+
+    public static void Main(string[] args) {
+        Calculate del = Add;
+        Console.WriteLine(del(10,10));
+
+        Calculate del2 = Subtract;
+        Console.WriteLine(del2(10, 10));
+
+
+        DiscountStrategy del3 = FestivalDiscount;
+        Console.WriteLine(del3(100));
+
+        DiscountStrategy del4 = SeasonalDiscount;
+        Console.WriteLine(del4(100));
+
+
+        DiscountStrategy del5 = NoDiscount;
+        Console.WriteLine(del5(100));
+
+
+
+
+    }
+} 
